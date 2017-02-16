0c25afd8ab9810ebe00aeebe738e126039308901
makena <roselynemakena@gmail.com>
2017-02-16 18:18:22 +0300

    Change schema name

---
+++ instance/configmodule.py
@@ -0,0 +1,25 @@
+class Config(object):
+    DEBUG = False
+    SECRET_KEY = 'caFGG!eg%$$@SEGf'
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'postgres://xkfnfnolmwtecu:97e92be31c16f530e1c9eae4065b67bbec563aaa7368e691fb5516e708f84578@ec2-107-20-191-76.compute-1.amazonaws.com:5432/ddkl07tsnsinfr'
+    
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAKENA = "roselyne"
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql://admin:root@localhost/library'
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+app_config = {
+	'development' : DevelopmentConfig,
+	'production': ProductionConfig
+
+}
